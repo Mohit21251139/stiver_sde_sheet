3f810f3f56d9d8f98db427088607dbc480a9daf4
Mohit Saini <104017316+Mohit21251139@users.noreply.github.com>
2024-02-14 21:06:42 +0530

    Time: 39 ms (42.55%), Space: 21.4 MB (31.91%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div875-koko-eating-bananas/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div875-koko-eating-bananas.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    private:
+    int findMax(vector <int>& piles){
+        int maxi = INT_MIN;
+        int n = piles.size();
+        for (int i = 0;i<n;i++){
+            maxi = max(maxi,piles[i]);
+        }
+        return maxi;
+    }
+    long long totalhrs(vector<int>& piles ,int hrs){
+        long long  totalhrs = 0;
+        int n = piles.size();
+        for (int i = 0;i< n;i++){
+            totalhrs += ceil((double)piles[i]/(double)hrs);
+        }
+        return totalhrs;
+    }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        long long low = 1,high =findMax(piles);
+        while(low <= high){
+            long long mid = (low + high)/2;
+            long long  hrs = totalhrs(piles,mid);
+            if (hrs<= h){
+                high = mid -1;
+            }
+            else{
+                low = mid +1;
+            }
+        }
+        return low;
+    }
+};
