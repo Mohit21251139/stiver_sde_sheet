8a3f6af8442315dbb8d107d98179c9a93a801a45
Mohit Saini <104017316+Mohit21251139@users.noreply.github.com>
2024-07-08 08:05:59 +0530

    Time: 0 ms (100%), Space: 7.2 MB (62.2%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        queue<int> st;
-        for(int i = 1;i<=n;i++)st.push(i);
-        while (st.size()!= 1){
-            for (int i = 0;i<k-1;i++){
-                int elem = st.front();
-                st.pop();
-                st.push(elem);
-            }
-            if (st.size() == 1)break;
-            st.pop();
+        // queue<int> st;
+        // for(int i = 1;i<=n;i++)st.push(i);
+        // while (st.size()!= 1){
+        //     for (int i = 0;i<k-1;i++){
+        //         int elem = st.front();
+        //         st.pop();
+        //         st.push(elem);
+        //     }
+        //     if (st.size() == 1)break;
+        //     st.pop();
+        // }
+        // return st.front();
+
+        //CP solution
+        int cnt = 1;
+        for (int i =1;i<= n;i++){
+            cnt = (cnt + k)%i;
         }
-        return st.front();
+        return cnt +1;
     }
 };
