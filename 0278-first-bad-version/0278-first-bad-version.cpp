a88d47289fa8e2b732f317477e5d77e11953db6f
Mohit Saini <104017316+Mohit21251139@users.noreply.github.com>
2024-07-18 09:00:58 +0530

    Time: 2 ms (58.39%), Space: 7.6 MB (7.37%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -5,16 +5,34 @@ class Solution {
 public:
     
     int firstBadVersion(int n) {
-        int m = 0;
-        for (int i = 1;i<=n;i++){
-            if (isBadVersion(i)){
-                m =  i;
-                break;
-            }
-            else{
-                continue;
-            }
+        //t.c =0(n)
+        // int m = 0;
+        // for (int i = 1;i<=n;i++){
+        //     if (isBadVersion(i)){
+        //         m =  i;
+        //         break;
+        //     }
+        //     else{
+        //         continue;
+        //     }
+        // }
+        // return m;
+        
+        //BINARY SEARCH
+        //T.C=0(logn)
+        long long l = 0,r= n;
+        long long m = 0;
+        while (l<=r){
+           long long  mid = l+(r-l)/2;
+           if (isBadVersion(mid)){
+            m = mid;
+            r= mid - 1;
+           }
+           else{
+            l = mid +1;
+           }
         }
         return m;
+        
     }
 };
