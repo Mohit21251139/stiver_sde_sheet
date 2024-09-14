4098a8fc081c37d0d80d03ae0f0ea9dced0d4afa
Mohit Saini <104017316+Mohit21251139@users.noreply.github.com>
2024-09-14 19:55:30 +0530

    Time: 2 ms (84.29%), Space: 9.6 MB (75.95%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,27 +1,7 @@
 class Solution {
 public:
-   bool isafe (int n,int i,int j , vector<string>& board){
-    int row = i,col = j;
-    //left side
-    while (row>-1 && col>-1 ){
-        if (board[row][col] == 'Q' ){
-            return 0;
-
-        }
-        row --,col --;
-    }
-    //right side
-    row = i,col = j;
-    while (row>-1 && col < n){
-        if (board[row][col] == 'Q'){
-            return 0;
-        }
-        row--,col ++;
-
-    }
-    return 1;
-   }
-void sol(int r ,int n, vector<vector<string>>& ans, vector<string>& board,vector<int> &col){
+   
+void sol(int r ,int n, vector<vector<string>>& ans, vector<string>& board,vector<int> &col,  vector<int>& leftdig,  vector<int> &rightdig){
        //base condition
        if (r == n){
         ans.push_back(board);
@@ -29,12 +9,16 @@ void sol(int r ,int n, vector<vector<string>>& ans, vector<string>& board,vector
        }
 
        for (int c = 0;c < n ;c++){
-        if (col[c] == 0 && isafe(n,r,c,board)){
+        if (col[c] == 0 && leftdig[n-1+c-r] == 0 && rightdig[r+c]==0){
              col[c] = 1;
              board[r][c] = 'Q';
-             sol(r+1,n,ans,board,col );
+             leftdig[n-1+c-r] = 1;
+             rightdig[r+c] = 1;
+             sol(r+1,n,ans,board,col ,leftdig,rightdig);
              col[c ]= 0;
              board[r][c]= '.';
+               leftdig[n-1+c-r] = 0;
+             rightdig[r+c] = 0;
         }
        }
 
@@ -46,8 +30,10 @@ void sol(int r ,int n, vector<vector<string>>& ans, vector<string>& board,vector
         for (int j = 0;j< n;j++)
         board[i].push_back('.');
         vector<vector<string>> ans;
+        vector<int> leftdig(2*n-1,0);
+        vector<int> rightdig(2*n-1,0);
         vector<int> column(n,0);
-        sol (0,n, ans ,board,column);
+        sol (0,n, ans ,board,column,leftdig,rightdig);
         return ans;
     }
 };
