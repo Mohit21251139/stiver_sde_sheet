e26550cde4f2c3a613c54eafb9dcf55ffdb9fc58
Mohit Saini <104017316+Mohit21251139@users.noreply.github.com>
2024-09-29 11:22:20 +0530

    Time: 97 ms (56.07%), Space: 55 MB (70.06%) - LeetHub

---
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -0,0 +1,44 @@
+class AllOne {
+public:
+   unordered_map<string,int> mpp;
+   set<pair<int,string>> st;
+
+    AllOne() {
+       mpp.clear();
+    }
+    
+    void inc(string key) {
+        int n = mpp[key];
+        mpp[key] ++;
+        st.erase({n,key});
+        st.insert({n+1,key});
+    }
+    
+    void dec(string key) {
+      int n = mpp[key];
+      mpp[key]--;
+      st.erase ({n,key});
+      if (mpp[key] > 0 )st.insert({n-1,key});
+      else mpp.erase(key);
+    }
+    
+    string getMaxKey() {
+      if (!st.empty())return st.rbegin()-> second;
+      return "";
+    }
+    
+    string getMinKey() {
+        if (!st.empty())return st.begin() -> second;
+        return "";
+        
+    }
+};
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne* obj = new AllOne();
+ * obj->inc(key);
+ * obj->dec(key);
+ * string param_3 = obj->getMaxKey();
+ * string param_4 = obj->getMinKey();
+ */
