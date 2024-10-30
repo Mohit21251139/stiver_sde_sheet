c81d59f615b929f352d4f40dad1137d742c78a3c
Mohit Saini <104017316+Mohit21251139@users.noreply.github.com>
2024-10-30 09:33:20 +0530

    Time: 9 ms (87.81%), Space: 74.5 MB (26.45%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
+    int sol(int i ,int j,vector<vector<int>>& grid,vector<vector<int>>& dp){
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        if (i >= n || j >= m)return 0;
+        if (dp[i][j] != -1 )return dp[i][j];
+        int maxi = 0;
+       if (i-1 >= 0 && j+1 < m && grid[i-1][j+1] > grid[i][j]){
+        maxi =max(maxi,1+sol(i-1,j+1,grid,dp));
+       }
+        if ( j+1 < m && grid[i][j+1] > grid[i][j]){
+        maxi = max(maxi,1+sol(i,j+1,grid,dp));
+       }
+        if ( i+1<n && j+1 < m && grid[i+1][j+1] > grid[i][j]){
+        maxi = max(maxi,1+sol(i+1,j+1,grid,dp));
+       }
+     return dp[i][j]= maxi;
+    }
     int maxMoves(vector<vector<int>>& grid) {
-               int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        int maxMoves = 0;
-
-        for (int col = n - 2; col >= 0; --col) {
-            for (int row = 0; row < m; ++row) {
-                if (row > 0 && grid[row][col] < grid[row - 1][col + 1]) {
-                    dp[row][col] = max(dp[row][col], dp[row - 1][col + 1] + 1);
-                }
-                if (grid[row][col] < grid[row][col + 1]) {
-                    dp[row][col] = max(dp[row][col], dp[row][col + 1] + 1);
-                }
-                if (row < m - 1 && grid[row][col] < grid[row + 1][col + 1]) {
-                    dp[row][col] = max(dp[row][col], dp[row + 1][col + 1] + 1);
-                }
-            }
+        int ans = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n,vector<int> (m,-1));
+        for (int i =0;i<n;i++){
+            ans = max(ans,sol(i,0,grid,dp));
         }
-
-        for (int row = 0; row < m; ++row) {
-            maxMoves = max(maxMoves, dp[row][0]);
-        }
-        return maxMoves;
+        return ans;
     }
 };
