2ae3be7ec68c2a383eefe219cbae270d1ce5a935
Mohit Saini <104017316+Mohit21251139@users.noreply.github.com>
2026-05-22 15:56:28 +0530

    Time: 497 ms (52.22%), Space: 0B (100%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT player_id, MIN(event_date) as first_login FROM Activity group by player_id order by event_date
+SELECT player_id, MIN(event_date) as first_login FROM Activity group by player_id order by player_id;
