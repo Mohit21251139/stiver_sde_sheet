1f5b39149d0cf39818687a40653d7cf529280ee3
Mohit Saini <104017316+Mohit21251139@users.noreply.github.com>
2026-05-26 22:23:56 +0530

    Time: 75 ms (85.34%), Space: 11 MB (82.43%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n = s.length();
+          int ans = 0;
+          for (int i =0;i< n;i++){
+            vector <int> freq(26,0);
+            int maxFreq = 0;
+
+            for (int j = i ; j< n;j++){
+                  int indx = s[j] - 'a';
+                  freq[indx] ++;
+                  maxFreq = max(maxFreq,freq[indx]);
+                  int minFreq = INT_MAX;
+                  for (int k = 0 ; k< 26;k++){
+                     if (freq[k] > 0){
+                        minFreq = min(minFreq,freq[k]);
+                     }
+
+                  }
+                  ans += maxFreq - minFreq;
+
+            }
+          }
+          return ans;
+    }
+};
