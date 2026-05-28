777a6ababa0f8f33b8fc81f3505b17684d5a97b3
Mohit Saini <104017316+Mohit21251139@users.noreply.github.com>
2026-05-28 22:23:03 +0530

    Time: 0 ms (100%), Space: 9.2 MB (81.08%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-       if (s.size()!= t.size()){
-        return false;
-       }
-       int sarr[256] = {0};
-       int tarr[256] = {0};
-       int n = s.size();
-       for (int i = 0;i<n;i++){
-        if (sarr[s[i]] != tarr[t[i]]){
-            return false;
+        if (s.length() != t.length())return false;
+        int count[256] = {0};
+        int count1[256] = {0};
+        for (int i = 0;i< s.length();i++){
+            if (count[s[i]] != count1[t[i]])return false;
+            count[s[i]] = i+1;
+            count1[t[i]]= i+1;
         }
-        sarr[s[i]] = i+1;
-        tarr[t[i]] = i+1;
-       }
-       return true;
-        
+        return true;
     }
 };
