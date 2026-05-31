79331366a61aec48764581aa338b083f9d35ff49
Mohit Saini <104017316+Mohit21251139@users.noreply.github.com>
2026-05-31 16:24:43 +0530

    Time: 1434 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+ SELECT ROUND(sum(tiv_2016),2) as tiv_2016 FROM Insurance WHERE tiv_2015 IN (SELECT tiv_2015 FROM Insurance group by tiv_2015 having count(*) > 1) AND (lat,lon) IN  (SELECT lat,lon FROM Insurance group by lat,lon having count(*) = 1); 
