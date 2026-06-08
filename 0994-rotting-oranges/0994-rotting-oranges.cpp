23cd9c435ed360c9fdf90a8654bf1acedafbb5d4
Mohit Saini <104017316+Mohit21251139@users.noreply.github.com>
2026-06-08 21:36:48 +0530

    Time: 3 ms (28.03%), Space: 16.8 MB (69.17%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid[0].size();
+        int n = grid.size();
+        queue<pair<pair<int,int>, int>> q;
+        int vis[n][m];
+        for (int i = 0;i<n;i++){
+            for (int j = 0;j<m;j++){
+                if (grid[i][j] == 2){
+                    q.push({{i,j},0});
+                    vis[i][j] = 2;
+                }
+                else{
+                    vis[i][j] = 0;
+                }
+            }
+        }
+
+         int tm = 0;
+         int drow[] = {-1,0,+1,0};
+         int dcol[] = {0,+1,0,-1};
+         while(!q.empty()){
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int t = q.front().second;
+            tm = max(tm,t);
+            q.pop();
+            for ( int i = 0;i< 4 ;i++){
+                int rrow = row + drow[i];
+                int ccol = col + dcol[i];
+
+                 if (rrow >= 0 && rrow < n && ccol >= 0 && ccol < m && vis[rrow][ccol]== 0 && grid[rrow][ccol] == 1){
+                  q.push({{rrow,ccol},t + 1});
+                  vis[rrow][ccol] = 2;
+            }
+
+            }
+           
+         }
+         for (int i = 0;i< n;i++){
+            for (int j = 0;j< m;j++){
+                if (grid[i][j] == 1 && vis[i][j] == 0){
+                    return -1;
+                } 
+            }
+         }
+          return tm;
+    }
+};
