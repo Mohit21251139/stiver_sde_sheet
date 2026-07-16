232074cbea709ce7f14eff050deea470a041d750
Mohit Saini <104017316+Mohit21251139@users.noreply.github.com>
2026-07-16 17:30:52 +0530

    Time: 1 ms (21.17%), Space: 8.5 MB (87.47%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-        return pow(x,0.5);
+    int mySqrt(int n) {
+        long long int s = 0;
+        long long int e = n;
+        long long int mid = s +(e-s)/2;
+        long long int ans  = -1;
+        while (s <= e){
+            long long int temp = mid * mid ;
+            if (temp == n)return mid;
+            else if (temp < n){
+                ans = mid;
+                s = mid +1;
+
+            }
+            else{
+                e = mid -1;
+            }
+            mid = s + (e-s)/2;
+        } 
+        return ans;
+
     }
 };
